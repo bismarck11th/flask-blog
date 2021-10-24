4d93ad57d14b1168182f50be38c531e0d0769171
kazuki827 <xm.kazuki827@gmail.com>
2021-10-24 14:26:17 +0900

    Add authentication function

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
-from flask import render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import pytz
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config['SECRET_KEY'] = os.urandom(24)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(12))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Post(db.Model):
@@ -19,13 +37,52 @@ class Post(db.Model):
 
 
 @app.route("/", methods=['GET', 'POST'])
+@login_required
 def index():
     if request.method == 'GET':
         posts = Post.query.all()
         return render_template("index.html", posts=posts)
 
 
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User(username=username, password=generate_password_hash(password, method='sha256'))
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/login')
+    else:
+        return render_template("signup.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/')
+    else:
+        return render_template("login.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
+
 @app.route("/create", methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == 'POST':
         title = request.form.get('title')
@@ -42,6 +99,7 @@ def create():
 
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
     post = Post.query.get(id)
     if request.method == 'GET':
@@ -57,6 +115,7 @@ def update(id):
 
 
 @app.route('/delete/<int:id>', methods=['GET'])
+@login_required
 def delete(id):
     post = Post.query.get(id)
 
---
+++ memo.md
@@ -0,0 +1,12 @@
+# SQLAlchemy
+
+| | SQLツールキット | ORM |
+| :---: | :---: | :---: |
+| pythonライブラリ | SQLAlchemy (Core) | SQLAlchemy (ORM) |
+| flask用拡張機能 | flask-sqlalchemy-core | flask-sqlalchemy |
+
+## SQLツールキット
+主な目的は、PythonからSQLクエリ文をPythonで使えるようにすること。
+
+## ORM
+主な目的は、Pythonのコードで、SQLなどのデータベースを操作すること。
